ecdda6a8378f141e2b58b2183734c2332397415d
RG1ee <niwows@yandex.ru>
2023-06-16 21:07:42 +0300

    create index, views and settings router with asgi

--- src/core/urls.py
+++ src/core/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
 
 urlpatterns = [
+    path("chat/", include("src.apps.chat.urls")),
     path("admin/", admin.site.urls),
 ]
